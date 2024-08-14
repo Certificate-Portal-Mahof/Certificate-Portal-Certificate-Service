f6c66564b71561ecd23a061e68975eb5df0fc9ec
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-14 11:26:14 +0300

    Add return type to file_names functions

--- tools/file_names.py
+++ tools/file_names.py
@@ -1,6 +1,7 @@
 import uuid
 from config import TEMP_FILES_FOLDER_PATH
 
+
 class FileNames:
     file_name: str = ""
 
@@ -8,26 +9,26 @@ class FileNames:
         unique_id = uuid.uuid4()
         self.file_name = f"{prefix}{unique_id}"
 
-    def get_key_filename(self):
+    def get_key_filename(self) -> str:
         return self.file_name + ".key"
 
-    def get_key_filepath(self):
+    def get_key_filepath(self) -> str:
         return TEMP_FILES_FOLDER_PATH + "/key_files/" + self.get_key_filename()
 
-    def get_csr_filename(self):
+    def get_csr_filename(self) -> str:
         return self.file_name + ".csr"
 
-    def get_csr_filepath(self):
+    def get_csr_filepath(self) -> str:
         return TEMP_FILES_FOLDER_PATH + "/csr_files/" + self.get_csr_filename()
 
-    def get_crt_filename(self):
+    def get_crt_filename(self) -> str:
         return self.file_name + ".crt"
 
-    def get_crt_filepath(self):
+    def get_crt_filepath(self) -> str:
         return TEMP_FILES_FOLDER_PATH + "/crt_files/" + self.get_crt_filename()
 
-    def get_ext_filename(self):
+    def get_ext_filename(self) -> str:
         return self.file_name + ".v3.ext"
 
-    def get_ext_filepath(self):
+    def get_ext_filepath(self) -> str:
         return TEMP_FILES_FOLDER_PATH + "/ext_files/" + self.get_ext_filename()
