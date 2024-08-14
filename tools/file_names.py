1423497fe98384a8e55490ed20cfcb57edba72cb
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-14 11:27:04 +0300

    Added pem file functions and an all file paths function to file_names.py

--- tools/file_names.py
+++ tools/file_names.py
@@ -32,3 +32,18 @@ class FileNames:
 
     def get_ext_filepath(self) -> str:
         return TEMP_FILES_FOLDER_PATH + "/ext_files/" + self.get_ext_filename()
+
+    def get_pem_filename(self) -> str:
+        return self.file_name + ".pem"
+
+    def get_pem_filepath(self) -> str:
+        return TEMP_FILES_FOLDER_PATH + "/pem_files/" + self.get_pem_filename()
+
+    def get_all_files_paths(self) -> list[str]:
+        return [
+            self.get_csr_filepath(),
+            self.get_key_filepath(),
+            self.get_ext_filepath(),
+            self.get_crt_filepath(),
+            self.get_pem_filepath()
+        ]
