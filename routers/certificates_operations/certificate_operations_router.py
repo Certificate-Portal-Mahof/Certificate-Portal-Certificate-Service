bd9028a38bdd37772dba8ce89b907930a6ef8aab
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-14 12:27:24 +0300

    Created a route of certificate creation. Implemented service and router files

--- routers/certificates_operations/certificate_operations_router.py
+++ routers/certificates_operations/certificate_operations_router.py
@@ -1,17 +1,14 @@
-from typing import Annotated
-
 from litestar import Router, post
-from litestar.params import Body
-from models.certificate_fields import CertificateFields
-from tools.utils.certificate_operations_utils import CertificateOperationsUtils
+from litestar.response import Response
+from models.certificate_data import CertificateData
+from routers.certificates_operations.certificate_operations_service import CertificateOperationsService, remove_files
 
 
 @post("/create-certificate")
-async def create_certificate(data: CertificateFields) -> None:
-    print(type(data))
-    print(data.dns_names)
-
-    CertificateOperationsUtils().create_certificate(certificate_fields=data)
+async def create_certificate(data: CertificateData) -> Response:
+    create_certificate_background_task = await CertificateOperationsService().create_certificate(data)
+    return Response(content="Certificate Creation Submitted Successfully",
+                    background=create_certificate_background_task)
 
 
 router = Router(path="/cert-ops", route_handlers=[create_certificate])
---
+++ routers/certificates_operations/certificate_operations_service.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+
+import aiofiles
+
+from models.certificate_data import CertificateData
+from tools.rabbitmq.rabbitmq_manager import RabbitMQManager
+from tools.rabbitmq.rabbitmq_utils import RabbitMQUtils
+from tools.utils.certificate_operations_utils import CertificateOperationsUtils
+from litestar.background_tasks import BackgroundTask
+from tools.file_names import FileNames
+
+
+async def remove_files(certificate_filenames: FileNames) -> None:
+    event_loop = asyncio.get_event_loop()
+    try:
+        tasks = [event_loop.run_in_executor(None, os.remove, path) for path in
+                 certificate_filenames.get_all_files_paths()]
+        await asyncio.gather(*tasks)
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+
+
+class CertificateOperationsService:
+    def __init__(self) -> None:
+        self.certificate_operation_utils = CertificateOperationsUtils()
+        self.rabbitmq_manager = RabbitMQManager()
+
+    async def create_certificate(self, data: CertificateData) -> BackgroundTask:
+        return BackgroundTask(self.create_certificate_in_background, data)
+
+    async def create_certificate_in_background(self, data: CertificateData) -> None:
+        certificate_filenames: (FileNames | None) = await self.certificate_operation_utils.create_certificate(data)
+
+        if certificate_filenames is None:
+            return
+
+        async with aiofiles.open(certificate_filenames.get_pem_filepath(), "rb") as file:
+            file_bytes = await file.read()
+        queue_message = str.encode(data.certificate_id + " ") + file_bytes
+
+        await remove_files(certificate_filenames)
+
+        await RabbitMQUtils().send_message(queue_message=queue_message)
