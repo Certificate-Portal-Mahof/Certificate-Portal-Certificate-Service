8d8c413f45f8e68c2398faf58afefd860c0fd39c
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-21 10:39:23 +0300

    Added an upload certificate route and implemented the service for it

--- routers/certificates_operations/certificate_operations_router.py
+++ routers/certificates_operations/certificate_operations_router.py
@@ -1,8 +1,13 @@
-from litestar import Router, post
 from typing import Annotated
+
+from litestar import Router, post, MediaType, status_codes
+from litestar.enums import RequestEncodingType
+from litestar.params import Body
 from litestar.response import Response
 
 from models.certificate_data import CertificateDataCertId
+from models.certificate_file import CertificateFile
+from routers.certificates_operations.certificate_operations_service import CertificateOperationsService
 
 
 @post("/create-certificate", status_code=status_codes.HTTP_200_OK)
@@ -12,4 +17,13 @@ async def create_certificate(data: Annotated[CertificateDataCertId, Body()]) -> 
                     background=create_certificate_background_task)
 
 
-router = Router(path="/cert-ops", route_handlers=[create_certificate])
+@post("/upload-certificate", status_code=status_codes.HTTP_200_OK, media_type=MediaType.TEXT)
+async def upload_certificate(data: Annotated[CertificateFile, Body(media_type=RequestEncodingType.MULTI_PART)],
+                             ) -> Response:
+    upload_certificate_background_task = await CertificateOperationsService().upload_certificate(data)
+
+    return Response(content="Certificate Uploading Submitted Successfully",
+                    background=upload_certificate_background_task)
+
+
+router = Router(path="/cert-ops", route_handlers=[create_certificate, upload_certificate])
--- routers/certificates_operations/certificate_operations_service.py
+++ routers/certificates_operations/certificate_operations_service.py
@@ -1,4 +1,5 @@
 from models.certificate_data import CertificateDataCertId, CertificateDataUserId
+from models.certificate_file import CertificateFile
 from routers.certificates_operations.certificate_operations_repo import CertificateOperationsRepo
 from tools.utils.certificate_operations_utils import CertificateOperationsUtils
 from litestar.background_tasks import BackgroundTask
@@ -22,3 +23,22 @@ class CertificateOperationsService:
     async def create_certificate(self, data: CertificateDataCertId) -> BackgroundTask:
         return BackgroundTask(self.__create_certificate_in_background, data)
 
+    async def __upload_certificate_in_background(self, certificate_file: CertificateFile) -> None:
+        pem_content = await certificate_file.pem_file.read()
+
+        certificate_metadata = await self.certificate_operation_utils.convert_certificate_to_object(pem_content)
+        if certificate_metadata is None:
+            print("Certificate Upload Failed")
+            return
+
+        user_id = certificate_file.user_id
+        cert_name = certificate_file.cert_name
+        certificate_data = CertificateDataUserId(user_id=user_id, cert_name=cert_name,
+                                                 **(certificate_metadata.model_dump()))
+
+        certificate_object_id = await self.repo.create_certificate(certificate_data)
+        certificate_id = str(certificate_object_id)
+        await self.repo.upload_certificate_file(certificate_id=certificate_id, file_bytes=pem_content)
+
+    async def upload_certificate(self, certificate_file: CertificateFile) -> BackgroundTask:
+        return BackgroundTask(self.__upload_certificate_in_background, certificate_file)
