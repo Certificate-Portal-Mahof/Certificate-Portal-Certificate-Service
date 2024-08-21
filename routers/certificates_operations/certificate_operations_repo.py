ef5722cbb98d17d2a3a60a7dad819bc2ad8f2ef6
Maxim Gavrilove <maximgav123@gmail.com>
2024-08-21 10:33:22 +0300

    Added a method to the repo to upload a certificate to the db

--- routers/certificates_operations/certificate_operations_repo.py
+++ routers/certificates_operations/certificate_operations_repo.py
@@ -1,3 +1,8 @@
+from bson import ObjectId
+from litestar import status_codes
+from litestar.exceptions import HTTPException
+from pymongo.errors import DuplicateKeyError
+
 from db.db import MongoConnector
 from models.certificate_data import CertificateDataUserId
 
@@ -9,3 +14,10 @@ class CertificateOperationsRepo:
 
     async def upload_certificate_file(self, certificate_id: str, file_bytes: bytes) -> None:
         await self.fs.upload_from_stream(filename=certificate_id, source=file_bytes)
+
+    async def create_certificate(self, certificate: CertificateDataUserId) -> ObjectId:
+        try:
+            new_certificate_details = await self.db["certificates"].insert_one(certificate.model_dump())
+            return new_certificate_details.inserted_id
+        except DuplicateKeyError:
+            raise HTTPException(status_code=status_codes.HTTP_409_CONFLICT, detail="Certificate already exists.")
