8fd71e8eb60e6ab8b4af1abd83c4fb3faf2c5f11
Maxim Gavrilove <maximgav123@gmail.com>
2024-09-04 20:41:18 +0300

    Added additional settings

--- settings.py
+++ settings.py
@@ -10,7 +10,14 @@ class Settings(BaseSettings):
     host: str = "127.0.0.1" if is_dev else "0.0.0.0"
     port: int = 9200
     reload: bool = False
+
     allow_origins: List[str] = ["http://localhost:5173"] if is_dev else ["*"]
     allow_methods: List[str] = ["*"]
     allow_credentials: bool = True
     allow_headers: List[str] = ["*"]
+
+    root_ca_certificate_path = "./root_ca_files/RootCA.crt"
+    root_ca_key_path = "./root_ca_files/RootCA.key"
+    root_ca_passcode = os.getenv("ROOT_CA_PASSCODE")
+
+settings = Settings()
